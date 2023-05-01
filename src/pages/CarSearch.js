aab5d6ec1df72cf1759349bbe5f572a5518e452e
siyh22 <pooh9609@naver.com>
2023-05-01 19:36:50 +0900

    CarSearch

--- src/pages/CarSearch.js
+++ src/pages/CarSearch.js
@@ -1,1 +1,33 @@
-// test
+import React from "react";
+import Header from "../layout/Header";
+import Footer from "../layout/Footer";
+import styled from "styled-components";
+
+const CarSerachst = styled.body`
+     position: relative;
+    display: grid;
+   grid-template-columns: 1fr ;
+   grid-template-rows: repeat(2,minmax(140px,auto));
+
+`;
+
+
+
+const CarSerach = () => {
+
+    return (
+        <>
+        <Header/>
+        <div className="Container">
+            자동차 컨테이너 입니다.
+
+        </div>
+        <Footer/>
+        </>
+    );    
+
+
+
+}
+
+export default CarSerach;
