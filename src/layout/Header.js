52d5a2d2507354021a3fb362cd55364321e3ca99
김종연 <whddus426@gamil.com>
2023-05-01 19:29:19 +0900

    수정작업3

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -43,7 +43,7 @@ const Header = () => {
             <div >
             ECO FRIENDS
             </div>
-            {/* <LoginBtn/> */}
+            <LoginBtn/>
             
         </header>
         </Headerst>
