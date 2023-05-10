675615cd305fdf2116248400cb7d7f79ce6de1cc
김종연 <whddus426@gamil.com>
2023-05-10 21:42:08 +0900

    0510 9:42

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -13,7 +13,6 @@ const Footerst = styled.footer`
 
     @import url('https://fonts.googleapis.com/css2?family=Foldit:wght@300&display=swap');
 
-
     footer{
         flex-direction: column;
         align-items: center;
@@ -54,61 +53,60 @@ const Footerst = styled.footer`
    }
 `;
 
-const Footer = () => {
-
-
-    const customStyles = {
-        overlay: {
-          backgroundColor: 'rgba(0, 0, 0, 0.5)',
-        },
-        content: {
-          maxWidth: '500px',
-          margin: 'auto',
-          padding: '20px',
-          backgroundColor: '#fff',
-          borderRadius: '5px',
-        },
-      };
-
+      const Footer = () => {
+        
 
         const [isModalOpen, setIsModalOpen] = useState(false);
+        const [modalContents, setModalContents] = useState("");
       
-        const handleOpenModal = () => {
+        const handleOpenModal = (index) => {
           setIsModalOpen(true);
+          if (index === 0) {
+            setModalContents("개인정보처리방침 내용");
+          } else if (index === 1) {
+            setModalContents("이메일무단수집거부 내용");
+          } else if (index === 2) {
+            setModalContents("저작권정책 내용");
+          }
         };
       
         const handleCloseModal = () => {
           setIsModalOpen(false);
+          setModalContents("");
         };
-    return(
+        return(
         <Footerst>
-            <footer>
-                <div className="div1">
-                    <nav>
-                    <ul>
-                        <a onClick={handleOpenModal}>개인정보처리방침</a>
-                        <Modal 
-                        isOpen={isModalOpen} onClose={handleCloseModal} content="대표 홈페이지는 정보주체의 자유와 권리 보호를 위해 「개인정보 보호법」 및 관계 법령이 정한 바를 준수하여, 
-                        적법하게 개인정보를 처리하고 안전하게 관리하고 있습니다. 이에 「개인정보 보호법」 
-                        제30조에 따라 정보주체에게 개인정보 처리에 관한 절차 및 기준을 안내하고,
-                        이와 관련한 고충을 신속하고 원활하게 처리할 수 있도록 하기 위하여 다음과 같이 개인정보 처리방침을 수립·공개합니다."
-                        customStyles={customStyles}/>
-                        <a onClick={handleOpenModal}>저작권정책</a><Modal isOpen={isModalOpen} onClose={handleCloseModal} content="dfdfdfd"/>
-                        <a onClick={handleOpenModal}>이메일무단수집거부</a><Modal isOpen={isModalOpen} onClose={handleCloseModal} content="cfccdfdfd"/>
-                        <a onClick={handleOpenModal}>고객센터</a><Modal isOpen={isModalOpen} onClose={handleCloseModal} content="hhhhhhdfd"/>
-                    </ul>
-                    </nav>
-                </div>
-                <div className="div2">
-                    <h2>ECO FRIEND</h2>
-                </div>
-                <div className="addr">
-                <h3>서울특별시 강남구 테헤란로14길 6 3층  &nbsp;&nbsp;&nbsp;  Eco Friend 대표전화 1588-0000</h3>
-                </div>
-            </footer>
-            
-        </Footerst>
-    );
-}
+        <footer>
+          <div className="div1">
+            <nav>
+              <ul>
+                <li>
+                  <a onClick={() => handleOpenModal(0)}>개인정보처리방침</a>
+                </li>
+                <li>
+                  <a onClick={() => handleOpenModal(1)}>이메일무단수집거부</a>
+                </li>
+                <li>
+                  <a onClick={() => handleOpenModal(2)}>저작권정책</a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+          <div className="div2">
+            <h2>ECO FRIEND</h2>
+          </div>
+          <div className="addr">
+            <h3>
+              서울특별시 강남구 테헤란로14길 6 3층 &nbsp;&nbsp;&nbsp; Eco Friend
+              대표전화 1588-0000
+            </h3>
+          </div>
+        </footer>
 
-export default Footer;
+          <Modal isOpen={isModalOpen} onClose={handleCloseModal} 
+          contents={modalContents} title1={modalContents}/>
+      </Footerst>
+        );
+      };
+    
+  export default Footer;
--- src/utils/Modal.js
+++ src/utils/Modal.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import styled from 'styled-components';
 
-
-
  const Modalst = styled.div`
 .modal-overlay {
+    display: flex;
+    flex-direction: column;
     position : fixed;
     justify-content: center;
     top : 50%;
@@ -13,43 +13,55 @@ import styled from 'styled-components';
     transform: translate(-50%, -50%);
     width: 30%;
     height: 55%;
+    border: 1px solid black;
+}
+.title{
+  position: none;
+  top : 50%;
+  left: 50%;
+  justify-content: center;
 }
 .modal-content {
     justify-content: center;
+
 }
 .modal-content h2 {
     font-size: 40px;
 }
 .div1 {
-    font-size: 15px;
+    text-align: center;
+    font-size: 20px;
 }
 .div2 {
     font-size: 15px;
 }
+div {
+  display: flex;
+  justify-content: center;
+  margin-top: 40px;
+  border: 1px solid black;
+}
+/* div .btn1 {
+  display: flex;
+  justify-content: center;
+  margin-top: 40px;
+} */
 `;
 
-const Modal = ({ isOpen, onClose , content }) => {
+const Modal = ({ isOpen, onClose , contents, title1 }) => {
   if (!isOpen) {
     return null;
   }
 
   return ( 
-  
     <Modalst>
     <div className="modal-overlay">
+      <div className='title'>{title1}</div>
       <div className="modal-content">
-        <h2>{}</h2>
-        <div className='div1'>
-        <p>{content}</p>
-        </div>
-        {/* <div className='div2'>
-            <li>개인정보의 안전성 확보조치</li>
-            <p>공사 개인정보보호 총괄부서에서는 개인정보의 안전성 확보를 위해 다음과 같은 조치를 취하고 있습니다.
-                1. 관리적 조치 : 내부관리계획 수립·시행, 정기적 직원 교육
-                2. 기술적 조치 : 개인정보처리시스템 등의 접근권한 관리, 접근통제시스템 설치, 고유식별정보 등의 암호화, 보안프로그램 설치
-                3. 물리적 조치 : 전산실, 자료보관실 등의 접근통제</p>
-        </div> */}
-        <button onClick={onClose}>닫기</button>
+        <p>{contents}</p>
+      </div>
+      <div className='bottom'>
+      <button onClick={onClose}>닫기</button>
       </div>
     </div>
     </Modalst>
