846480cc7ca0aae5a1ca7abc408d2fd7b3eda853
sk <sungkeno3o@gmail.com>
2023-05-26 16:42:42 +0900

    버그수정

--- src/api/AxiosApi.js
+++ src/api/AxiosApi.js
@@ -93,6 +93,7 @@ const AxiosApi = {
     },
     // 충전금액조회
     getMoney : async(email) => {
+        console.log(email)
         return await axios.get(EF_DOMAIN + `/user/${email}/charge`);
     },
     
